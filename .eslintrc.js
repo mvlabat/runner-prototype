78a23afae644cb0b7dc6b2d15d4c82544713430b
mvlabat <mvlabat@gmail.com>
2018-03-21 15:22:23 +0200

    Initialize the project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+    'parserOptions': {
+        'ecmaVersion': 2017,
+        'sourceType': 'module',
+    },
+    'env': {
+        'browser': true,
+    },
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+# Platformio Game Front-end
+
+## Build
+```bash
+parcel index.html
+```
