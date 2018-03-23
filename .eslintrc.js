fcaeed1529d31402e918ef05607f461d91cff88b
mvlabat <mvlabat@gmail.com>
2018-03-23 23:33:38 +0200

    Improve .eslintrc.js rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,48 @@
 module.exports = {
-    "extends": "airbnb-base",
-    'parserOptions': {
-        'ecmaVersion': 2017,
-        'sourceType': 'module',
-    },
-    'env': {
-        'browser': true,
-    },
+  "extends": "airbnb-base",
+  'parserOptions': {
+    'ecmaVersion': 2017,
+    'sourceType': 'module',
+  },
+  'env': {
+    'browser': true,
+  },
+  'rules': {
+    quotes: ['error', 'single', { 'avoidEscape': true }],
+    'no-use-before-define': ['error', { 'functions': false }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'e', // for e.returnvalue
+        'rendererMesh',
+      ]
+    }],
+    'no-console': 'off',
+    'no-unused-vars': ["error", { "argsIgnorePattern": "^_" }],
+    'no-mixed-operators': ['error', {
+      groups: [
+        ['&', '|', '^', '~', '<<', '>>', '>>>'],
+        ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+        ['&&', '||'],
+        ['in', 'instanceof']
+      ],
+      allowSamePrecedence: true,
+    }],
+  },
 };
