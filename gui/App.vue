fd14f71d5ddd747479c5ee549523b0bc2caf324e
mvlabat <mvlabat@gmail.com>
2018-04-24 00:49:52 +0300

    Install Vue, add a GUI placeholder

---
+++ gui/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="canvas-wrapper">
+        <div id="gui-wrapper">
+            <div id="builder-bar">
+                <p>GUI placeholder</p>
+                <p>
+                    Press W/A/S/D to move camera or player,
+                    ESC to switch to player or builder mode.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App!',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+    html, body, #canvas-wrapper, #canvas-wrapper canvas {
+        display: block;
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    #canvas-wrapper {
+        position: relative;
+    }
+
+    #gui-wrapper {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        pointer-events: none;
+
+        > * {
+            pointer-events: all;
+        }
+    }
+
+    #builder-bar {
+        position: absolute;
+        bottom: 0;
+        height: 15%;
+        font-size: 2em;
+        text-align: center;
+        width: 100%;
+
+        background: rgba(0.2, 0.2, 0.2, 0.1);
+    }
+</style>
--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+import Vue from 'vue';
+import App from './gui/App.vue';
+
 import Game from './game/Game';
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  render: h => h(App),
+});
 
 Game();
