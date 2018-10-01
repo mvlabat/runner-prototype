a767a5931c5d3ad98d8dd973977ffeaf08d9579a
mvlabat <mvlabat@gmail.com>
2018-10-01 22:57:20 +0300

    Add helper install.sh and test.sh scripts

---
+++ install.sh
@@ -0,0 +1,4 @@
+cd common && yarn install
+cd ../backend && yarn install
+cd ../frontend && yarn install
+cd .. && yarn install
---
+++ test.sh
@@ -0,0 +1,4 @@
+cd common && yarn test
+cd ../backend && yarn test
+cd ../frontend && yarn test
+cd .. && yarn eslint
