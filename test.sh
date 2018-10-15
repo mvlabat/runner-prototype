d4d85979c5b6c3b72f4215f2a3a364b20f46eafc
mvlabat <mvlabat@gmail.com>
2018-10-15 14:18:23 +0300

    Make .travis.yml tests to be verbose and run as separate command (from build)

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-cd common && yarn test
-cd ../backend && yarn test
-cd ../frontend && yarn test
+cd common && yarn test --verbose
+cd ../backend && yarn test --verbose
+cd ../frontend && yarn test --verbose
 cd .. && yarn eslint
